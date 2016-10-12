76ee582c4e6168552c7a1e96a1045b2d94cfa88b
kellymersereau <kelly.mersereau@gmail.com>
2016-10-11 22:52:42 -0400

    fixed the user interface

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,58 +1,90 @@
+// on page load - runs function to create buttons
+$(function(){
+	renderButtons(tvShows, 'tvShowName', '#tvShowButtons');
+});
+
 var tvShows = ['Game of Thrones', 'The X Files', 'Dexter', 'Pretty Little Liars', 'Shameless', 'The Wire', 'Oz', 'The Newsroom', 'Weeds'];
 
-//creates the buttons for the TV shows 
-function renderButtons(){
-	$('#tvShowButtons').empty();
 
-	for(var i=0; i<tvShows.length; i++){
+//creates the buttons for the TV shows 
+function renderButtons(arrayToUse, classToAdd, areaToAddTo){
+	$(areaToAddTo).empty();
+
+	for(var i=0; i<arrayToUse.length; i++){
 		var a = $('<button>')
-		a.addClass('tvShow');
-		a.attr('data-name', tvShows[i]);
-		a.text(tvShows[i]);
-		$('#tvShowButtons').append(a);
+		a.addClass(classToAdd);
+		a.addClass('btn btn-default btn-lg');
+		a.attr('type', 'button');
+		a.attr('data-name', arrayToUse[i]);
+		a.text(arrayToUse[i]);
+		$(areaToAddTo).append(a);
 	}
 }
-//runs the render buttons function
-renderButtons();
 
 //creates the onclick function for the tv show buttons to create new tv show buttons based on user input
 $('#addTVShow').on('click', function(){
-	var tvShow = $('#tvShow-input').val().trim();
-	tvShows.push(tvShow);
-	renderButtons();
+	var newShow = $('#tvShow-input').val().trim();
+	$('#tvShow-input').val("");
+	tvShows.push(newShow);
+	renderButtons(tvShows, 'tvShowName', '#tvShowButtons');
 	return false;
 });
 
 //creates on click functions for tv show buttons
-$('.tvShow').on('click', function(){
+$(document).on('click', '.tvShowName', function(){
+	//this clears the previous choice before adding the new images
+	$('#tvShows').empty();
+	$('.tvShowName').removeClass('active');
+	$(this).addClass('active');
+
 	var show = $(this).data('name');
-	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=dc6zaTOxFJmzC&limit=10";
+	console.log('show', show);
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=dc6zaTOxFJmzC&limit=12&offset=12";
 
 	$.ajax({
 		url: queryURL,
 		method: 'GET'
 	})
 	.done(function(response) {
-		console.log(response)
+		console.log('response ', response)
 
 		var results = response.data;
 
 		for(var i=0; i < results.length; i++) {
-			var tvShowDiv = $('<div>');
-			var p = $('<h1>');
-			p.text(results[i].rating);
+			var tvShowDiv = $('<div class="tv-item col-sm-3">');
+			var rating = results[i].rating;
+
+			var p = $('<h1>').text("Rating: " + rating);
+			p.addClass('rating');
+			var animated = results[i].images.fixed_height.url;
+			var still = results[i].images.fixed_height_still.url;
+
 			var tvShowImage = $('<img>');
-			tvShowImage.attr('src', results[i].images.original_still.url);
-			tvShowImage.addClass('imageClick');
-			$(tvShowDiv).append(p);
-			$(tvShowDiv).append(tvShowImage);
-			$('#tvShows').prepend(tvShowDiv);
+			tvShowImage.addClass('img-responsive');
+			tvShowImage.attr('src', still);
+			tvShowImage.attr('data-still', still);
+			tvShowImage.attr('data-animate', animated);
+			tvShowImage.attr('data-state', 'still');
+			tvShowImage.attr('id', "tvShow-" + [i]);
+			tvShowImage.addClass('tvShowImage');
+
+			tvShowDiv.append(p)
+			tvShowDiv.append(tvShowImage)
+
+			$('#tvShows').append(tvShowDiv);
 		}
 	});
 });
-$('.imageClick').on('click', function(){
-	for(var i=0; i < results.length; i++) {
-		tvShowDiv.attr('src', results[i].images.original.url)
-		$(tvShowDiv).html(tvShowImage);
+
+$(document).on('click', '.tvShowImage', function(){
+	var state = $(this).attr('data-state');
+
+	if(state == 'still'){
+		$(this).attr('src', $(this).data('animate'));
+		$(this).attr('data-state', 'animate');
+	} else{
+		$(this).attr('src', $(this).data('still'));
+		$(this).attr('data-state', 'still');
 	}
 });
+
